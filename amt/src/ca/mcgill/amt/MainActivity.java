8f9d32bd751288ed9cff6df86d2fe8ae9425c03c
Ryan Singzon <rcsingzon@gmail.com>
2014-02-22 02:12:09 -0500

    Added test for no inputs

--- amt/src/ca/mcgill/amt/MainActivity.java
+++ amt/src/ca/mcgill/amt/MainActivity.java
@@ -3,9 +3,16 @@ package ca.mcgill.amt;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView errorBox;
+	private double firstSide, secondSide, thirdSide;
+	private EditText firstInput, secondInput, thirdInput;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +24,30 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/* Called after clicking on the "Get triangle" button
+	 * Checks the user input for errors and determines what type of 
+	 * triangle the user has input
+	 */
+	public void getTriangle(View v){
+	
+		//Obtain user input
+		firstInput = (EditText)findViewById(R.id.first_side);
+		secondInput = (EditText)findViewById(R.id.second_side);
+		thirdInput = (EditText)findViewById(R.id.third_side);
+
+		//Check if any of the inputs are empty
+		if(firstInput.getText().toString().equals("") 		||
+				secondInput.getText().toString().equals("")	||
+				thirdInput.getText().toString().equals("")	){
+			
+			//Displays an error message if any inputs are empty
+			errorBox = (TextView)findViewById(R.id.error_box);
+			errorBox.setText("Error: You must enter a value for each side");
+			errorBox.setVisibility(View.VISIBLE);
+		}
+		
 	}
 
 }
--- amtTest/src/ca/mcgill/amt/test/AmtTest.java
+++ amtTest/src/ca/mcgill/amt/test/AmtTest.java
@@ -1,3 +1,12 @@
+/**
+ * ECSE 428 - Software Engineering Practice
+ * Assignment B - Test Driven Development
+ * Winter 2014
+ * 
+ * Singzon, Ryan 	260397455
+ * Dao, Nhat-Quang 	260457711
+ */
+
 package ca.mcgill.amt.test;
 
 import org.junit.Ignore;
@@ -6,8 +15,11 @@ import org.junit.Test;
 import ca.mcgill.amt.R;
 import ca.mcgill.amt.MainActivity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AmtTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
@@ -31,6 +43,7 @@ public class AmtTest extends ActivityInstrumentationTestCase2<MainActivity>{
     	assertTrue(layout.getChildCount() > 0);
     }
     
+    
     @Test
     public void testInputsExist(){
     	int numChildren = layout.getChildCount();
@@ -42,7 +55,59 @@ public class AmtTest extends ActivityInstrumentationTestCase2<MainActivity>{
     		}
     	}
     	
-    	assertEquals(count, 3);
+    	assertEquals(3, count);
+    }
+   
+    @Test
+    public void testButtonExists(){
+    	int numChildren = layout.getChildCount();
+    	int count = 0;
+    	
+    	//Searches list and increments count for every input
+    	//field that is found
+    	for(int i = 0; i < numChildren; i++){
+    		if(layout.getChildAt(i) instanceof Button){
+    			count++;
+    		}
+    	}
+    	assertEquals(1, count);
     }
     
+    @Test
+    public void testNoInputsFilled(){
+    	int numChildren = layout.getChildCount();
+    	
+    	//Search for the button in the activity
+    	for(int i = 0; i < numChildren; i++){
+    		
+    		final View child = layout.getChildAt(i);
+    		
+    		//Click the button
+    		if(child instanceof Button){
+    			
+    			//Run button click on new thread
+    			try{
+    				runTestOnUiThread(new Runnable() {
+    					@Override
+    					public void run(){
+    						//Press the submit button
+    						Button button = (Button)child;
+    						button.performClick();
+    					}
+    				});
+    			} catch(Throwable e){
+    				
+    			}
+    		}
+    	}
+    	
+    	//Check if the error message appears
+    	String expectedMessage = "Error: You must enter a value for each side";
+    	String message = "";
+
+    	TextView errorBox = (TextView)layout.getChildAt(4);
+    	message = errorBox.getText().toString();
+    	   	
+    	assertEquals(expectedMessage, message);
+    }
 }
